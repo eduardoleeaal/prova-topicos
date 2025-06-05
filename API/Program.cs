b75dbb77365b9b6936e911cf6901a30ccfacaf70
EDUARDO LEAL <eduardo.leal@cs.up.edu.br>
2025-06-05 09:20:49 -0300

    Criação do GET

--- API/Program.cs
+++ API/Program.cs
@@ -8,7 +8,17 @@ builder.Services.AddDbContext<AppDataContext>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+//2-LISTAR TODOS
+app.MapGet("/api/tarefas", ([FromServices] AppDataContext ctx) => {
+    var tarefas = ctx.Tarefas.Include(t => t.Titulo).ToList();
+    if(tarefas.Any())
+    {
+        return Results.Ok(tarefas);
+    }
+
+    return Results.NotFound();
+});
 
 
 app.Run();
