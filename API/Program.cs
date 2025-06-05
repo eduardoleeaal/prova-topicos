da11ab104ab0d6b01bc4ffb7ae64eb0a537155f6
EDUARDO LEAL <eduardo.leal@cs.up.edu.br>
2025-06-05 09:36:47 -0300

    Adicionado POST

--- API/Program.cs
+++ API/Program.cs
@@ -3,11 +3,24 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDataContext>();
 
 var app = builder.Build();
 
+//1-POST
+app.MapPost("/api/tarefas/", ([FromBody] Tarefa tarefa, [FromServices] AppDataContext ctx) => 
+{
+    if(tarefa == null || (tarefa.Titulo.Length < 3))
+    {
+        return Results.BadRequest("Os requisitos para criar a tarefa não foram atendidos.");
+    }
+
+    ctx.Tarefas.Add(tarefa);
+    ctx.SaveChanges();
+    return Results.Created("", tarefa);
+});
 
 //2-LISTAR TODOS
 app.MapGet("/api/tarefas", ([FromServices] AppDataContext ctx) => {
@@ -20,5 +33,6 @@ app.MapGet("/api/tarefas", ([FromServices] AppDataContext ctx) => {
     return Results.NotFound();
 });
 
+//3-GET POR ID
 
 app.Run();
